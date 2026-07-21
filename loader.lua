15eed63e0f85fb38a687ff157f9a300ee9ac2fd9
myallsaresohuge-star <myallsaresohuge@gmail.com>
2026-07-21 00:10:35 -0400

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -2,7 +2,7 @@ local gameId = game.GameId
 if gameId == 1087859240 then
     pcall(function()
         loadstring(game:HttpGet(
-            "https://raw.githubusercontent.com/heisenburgah/HYDROXIDE/main/ROGUE/rogue_ui.lua",
+            "https://raw.githubusercontent.com/myallsaresohuge-star/HYDROXIDE1/main/ROGUE/rogue_ui.lua",
             true
         ))()
     end)
